6ac593bc7d560256b7b9c8ded27a32076fcec137
GunMetalBlack <powerhousegames206@gmail.com>
2023-02-14 18:34:27 -0500

    Init

---
+++ main.py
@@ -0,0 +1,39 @@
+import tcod
+
+from engine import Engine
+from entity import Entity
+from input_handlers import EventHandler
+
+#Default values
+def main() -> None:
+    screen_width = 80
+    screen_height = 50
+    
+    #sets the tileset resource
+    tileset = tcod.tileset.load_tilesheet(
+        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+    event_handler = EventHandler()
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
+    entities = {npc, player}
+    
+    engine = Engine(entities=entities, event_handler=event_handler, player=player)
+     
+    #Creates a new Screen with the given tile set and width and height
+    with tcod.context.new_terminal(
+        screen_width,
+        screen_height,
+        tileset=tileset,
+        title="ZENKAI",
+        vsync=True,
+    ) as context:
+        root_console = tcod.Console(screen_width, screen_height, order="F")
+        #Main game loop
+        while True:
+            engine.render(console=root_console, context=context)
+            events = tcod.event.wait()
+            engine.handle_events(events)
+
+if __name__ == "__main__":
+    main()
